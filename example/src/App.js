8a1e17b92869b981e125f48e89a382a173b1b890
Yanuar Waskito <yg.waskito@gmail.com>
2019-01-31 02:59:06 +0700

    Update example

--- example/src/App.js
+++ example/src/App.js
@@ -5,7 +5,7 @@ export default class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      src: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+      src: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
       poster: "https://raw.githubusercontent.com/waskito/react-modal-videojs/master/example/public/preview.png",
       show: false
     }
